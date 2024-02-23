810f85fa8b7f4751ba4b6672c9db246066980a3e
sebmaz <seb.maz93@icloud.com>
2024-02-23 06:39:56 +0400

    impl stack ts

--- src/ts/Stack.ts
+++ src/ts/Stack.ts
@@ -1,18 +1,51 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    private head: Node<T> | null;
 
     constructor() {
+        this.head = null;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = new Node(item);
 
-}
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
-        return undefined
-}
+        if (!this.head) {
+            return undefined;
+        }
+
+        if (this.length === 1) {
+            const head = this.head;
+            this.head = null;
+            return head.value;
+        }
+
+        const head = this.head;
+        this.head = head.prev;
+        return head.value;
+    }
+
     peek(): T | undefined {
-        return undefined
+        return this.head?.value;
+    }
 }
+
+class Node<T> {
+    value: T;
+    prev: Node<T> | null;
+
+    constructor(value: T) {
+        this.value = value;
+        this.prev = null;
+    }
 }
